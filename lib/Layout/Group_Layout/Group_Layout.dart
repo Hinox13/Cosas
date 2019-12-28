996cbdc75e1ea753569821b3be81544a419525fa
Guillo <gplanara@gmail.com>
2019-12-28 15:28:16 +0100

    Pintats els assets d'un grup (accés a subcolecions)

--- lib/Layout/Group_Layout/Group_Layout.dart
+++ lib/Layout/Group_Layout/Group_Layout.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:projecte_visual/Layout/Main_Screen/Widgets/Main_popupMenu.dart';
 import 'package:projecte_visual/Layout/User_Calendar/User_Calendar.dart';
+import 'package:projecte_visual/classes.dart';
+import 'package:projecte_visual/funcions.dart';
 
 class Group_Layout extends StatefulWidget {
   @override
@@ -10,10 +13,11 @@ class Group_Layout extends StatefulWidget {
 
 class _Group_LayoutState extends State<Group_Layout> {
   @override
+  var idgroup = 'iVOGHEIQKJpdFeWhnGrU';
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Group Name'),
+        title: Text('Group Name goes Here'),
         actions: <Widget>[
           //Widget para el desplegable add_group, Delete_group and profile
           Main_PopupMenu(),
@@ -22,20 +26,49 @@ class _Group_LayoutState extends State<Group_Layout> {
       //Codigo para la visualizacion del calendario acual
       floatingActionButton: FloatingActionButton(
         child: Icon(
-          Icons.add,
+          Icons.calendar_today,
           color: Colors.white,
         ),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => User_Calendar(),
+          ));
+        },
       ),
-      body: InkWell(
-        child: Container(
-          height: 200,
-          color: Colors.purple,
-          child: Text('Assetmos algo o que?'),
-        ),
-        onTap: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => User_Calendar()));
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('group')
+            .document(idgroup)
+            .collection('assets')
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          List<DocumentSnapshot> docs = snapshot.data.documents;
+          List<Asset> assets = getAssets(docs);
+          return ListView.builder(
+            itemCount: assets.length,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Group_Layout(assets[index].id),
+                  ));
+                },
+                child: ListTile(
+                  leading: Icon(
+                    Icons.account_circle,
+                    size: 40,
+                  ),
+                  title: Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Text(assets[index].name),
+                  ),
+                ),
+              );
+            },
+          );
         },
       ),
     );
